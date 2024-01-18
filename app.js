f51a17d1ee4d0bb69bb0dbe69c08a0de2495a5ab
vmdeom <vitormourao.dev@outlook.com>
2024-01-18 15:29:49 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const twitch = require('twitch');
+
+const app = express();
+
+app.listen(3000, '127.0.0.1');
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const twitch = require('twitch');
+
